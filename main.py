80d7963986cef1fc4500ccb307fb3958382b1a68
Yoshitaka Hisamatsu <yhissa0@gmail.com>
2022-09-21 15:40:37 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+render template (basic.html, heaer.html, block content, footer.html)
+use bootstrap
+"""
+import secrets
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
