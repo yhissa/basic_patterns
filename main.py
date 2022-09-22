e1fd77aa98dbaa06a0c1e7f69cafb31daf59a42e
Yoshitaka Hisamatsu <yhissa0@gmail.com>
2022-09-22 13:42:14 +0900

    add Message Flashing

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """
-render template (basic.html, heaer.html, block content, footer.html)
-use bootstrap
+# URL Route Registrations
+# render template (basic.html, heaer.html, block content, footer.html)
+# use bootstrap
+# Message Flashing
 """
 import secrets
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 from flask_bootstrap import Bootstrap
 
 
@@ -20,7 +22,18 @@ def home():
 
 @app.route('/redirect')
 def redirect_url_for():
-    return render_template("redirect_url_for.html")
+    return render_template("url_for.html")
+
+
+@app.route('/flash', methods=["GET", "POST"])
+def flash_message():
+    # Flashes a message to the next request.
+    # In order to remove the flashed message from the session and to display it to the user,
+    # the template has to call get_flashed_messages().
+    if request.method == "GET":
+        flash('flash message is displayed.')
+    return render_template("flash.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
